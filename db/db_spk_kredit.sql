6ec067d906ba3ad2a26aaa5075434aa118fab7bd
ibnu000 <38882801+ibnu1590@users.noreply.github.com>
2022-07-08 03:13:25 +0700

    Update db_spk_kredit.sql

--- db/db_spk_kredit.sql
+++ db/db_spk_kredit.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version 5.5.5-10.4.24-MariaDB)
-# Date: 2022-07-04 20:07:43
+# Date: 2022-07-08 03:13:12
 # Generator: MySQL-Front 6.0  (Build 2.20)
 
 
@@ -34,7 +34,7 @@ DROP TABLE IF EXISTS `hasil_akhir`;
 CREATE TABLE `hasil_akhir` (
   `id_calon_kr` int(11) DEFAULT NULL,
   `nama` varchar(50) DEFAULT NULL,
-  `nilai` float(11,2) DEFAULT NULL,
+  `nilai` float(11,1) DEFAULT NULL,
   `tanggal_lap` date DEFAULT NULL,
   `minggu` varchar(2) DEFAULT NULL,
   `bulan` varchar(10) DEFAULT NULL,
@@ -42,13 +42,13 @@ CREATE TABLE `hasil_akhir` (
   `keterangan` varchar(12) DEFAULT NULL,
   `id_akhir` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id_akhir`)
-) ENGINE=InnoDB AUTO_INCREMENT=2213 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2375 DEFAULT CHARSET=utf8mb4;
 
 #
 # Data for table "hasil_akhir"
 #
 
-INSERT INTO `hasil_akhir` VALUES (14,'Suyono',3.00,'2022-07-04','2','Jul','2017','Diterima',2263),(15,'Arief Wijaya',4.10,'2022-07-04','2','Jul','2017','Diterima',2264),(16,'Joko Suprapto',4.20,'2022-07-04','2','Jul','2017','Diterima',2265),(17,'Andre',3.00,'2022-07-04','2','Jul','2017','Diterima',2266),(18,'Stevi Fauzan',4.10,'2022-07-04','2','Jul','2017','Diterima',2267),(14,'Suyono',3.00,'2022-07-04','2','Jul','2018','Diterima',2268),(15,'Arief Wijaya',4.10,'2022-07-04','2','Jul','2018','Diterima',2269),(16,'Joko Suprapto',4.20,'2022-07-04','2','Jul','2018','Diterima',2270),(17,'Andre',3.00,'2022-07-04','2','Jul','2018','Diterima',2271),(18,'Stevi Fauzan',4.10,'2022-07-04','2','Jul','2018','Diterima',2272),(14,'Suyono',3.00,'2022-07-04','2','Jul','2019','Diterima',2273),(15,'Arief Wijaya',4.10,'2022-07-04','2','Jul','2019','Diterima',2274),(16,'Joko Suprapto',4.20,'2022-07-04','2','Jul','2019','Diterima',2275),(17,'Andre',3.00,'2022-07-04','2','Jul','2019','Diterima',2276),(18,'Stevi Fauzan',4.10,'2022-07-04','2','Jul','2019','Diterima',2277),(14,'Suyono',3.00,'2022-07-04','2','Jul','2022','Diterima',2278),(15,'Arief Wijaya',4.10,'2022-07-04','2','Jul','2022','Diterima',2279),(16,'Joko Suprapto',4.20,'2022-07-04','2','Jul','2022','Diterima',2280),(17,'Andre',3.00,'2022-07-04','2','Jul','2022','Diterima',2281),(18,'Stevi Fauzan',3.00,'2022-07-04','2','Jul','2022','Diterima',2282);
+INSERT INTO `hasil_akhir` VALUES (14,'Suyono',3.2,'2022-07-07','2','Jul','2022','Ditolak',2372),(15,'Arief Wijaya',3.2,'2022-07-07','2','Jul','2022','Ditolak',2373),(16,'Joko Suprapto',3.2,'2022-07-07','2','Jul','2022','Ditolak',2374);
 
 #
 # Structure for table "hasil_tpa"
@@ -59,19 +59,19 @@ CREATE TABLE `hasil_tpa` (
   `id_test` int(11) NOT NULL AUTO_INCREMENT,
   `id_calon_kr` int(11) DEFAULT NULL,
   `usia` int(11) DEFAULT NULL,
-  `gaji_perbulan` float(10,2) DEFAULT NULL,
-  `status_tempat_tinggal` float(10,2) DEFAULT NULL,
-  `pekerjaan` float(10,2) DEFAULT NULL,
-  `umur_motor` float(10,2) DEFAULT NULL,
+  `gaji_perbulan` float(10,1) DEFAULT NULL,
+  `status_tempat_tinggal` float(10,1) DEFAULT NULL,
+  `pekerjaan` float(10,1) DEFAULT NULL,
+  `umur_motor` float(10,1) DEFAULT NULL,
   PRIMARY KEY (`id_test`),
   KEY `id_calon_kr` (`id_calon_kr`)
-) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=138 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "hasil_tpa"
 #
 
-INSERT INTO `hasil_tpa` VALUES (83,14,119,124.00,129.00,138.00,143.00),(84,15,120,127.00,130.00,137.00,143.00),(85,16,119,125.00,131.00,135.00,141.00),(86,17,119,124.00,129.00,138.00,143.00),(87,18,119,124.00,129.00,138.00,143.00);
+INSERT INTO `hasil_tpa` VALUES (133,14,119,124.0,129.0,134.0,139.0),(136,15,119,124.0,129.0,134.0,139.0),(137,16,119,124.0,129.0,134.0,139.0);
 
 #
 # Structure for table "kreditur"
@@ -92,13 +92,13 @@ CREATE TABLE `kreditur` (
   `Jabatan` varchar(100) NOT NULL,
   `alasan` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id_calon_kr`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "kreditur"
 #
 
-INSERT INTO `kreditur` VALUES (14,'-','Suyono','Pria','Kampung Nrogtog pinang','089606853329','','1983-02-01','Jakarta','S1','SPV Gudang','Butuh uang untuk biaya persalinan'),(15,'-','Arief Wijaya','Pria','JL. Sejahtera No 15 Pinang Tangerang','089606877777','','1977-01-17','Jakarta','S1','Admin ','Butuh uang untuk pelunasan motor'),(16,'-','Joko Suprapto','Pria','JL Srengseng jakarta barat','089608989999','','1971-12-16','Jakarta','S1','Driver','Butuh uang untuk biaya sekolah anak'),(17,'-','Andre','Pria','JL peninggilan raya no 22','089789876656','','1992-02-13','Jakarta','S1','Staff','Butuh uang untuk bayar cicilan '),(18,'-','Stevi Fauzan','Pria','Pondok Jagung tangsel','089767675543','','1996-07-24','Jakarta','S1','Staff','Butuh uang untuk kebutuhan hidup'),(19,'-','asd','Pria','Jl. Ciledug Raya','089606854454','','1999-10-09','asd','SD','asd','asd');
+INSERT INTO `kreditur` VALUES (14,'-','Suyono','Wanita','Kampung Nrogtog pinang','089606853329','','1983-02-01','Jakarta','S1','SPV Gudang','Butuh uang untuk biaya persalinan'),(15,'-','Arief Wijaya','Pria','JL. Sejahtera No 15 Pinang Tangerang','089606877777','','1977-01-17','Jakarta','S1','Admin ','Butuh uang untuk pelunasan motor'),(16,'-','Joko Suprapto','Pria','JL Srengseng jakarta barat','089608989999','','1971-12-16','Jakarta','S1','Driver','Butuh uang untuk biaya sekolah anak'),(17,'-','Andre','Pria','JL peninggilan raya no 22','089789876656','','1992-02-13','Jakarta','S1','Staff','Butuh uang untuk bayar cicilan '),(18,'-','Stevi Fauzan','Pria','Pondok Jagung tangsel','089767675543','','1996-07-24','Jakarta','S1','Staff','Butuh uang untuk kebutuhan hidup'),(19,'-','asd','Pria','Jl. Ciledug Raya','089606854454','','1999-10-09','asd','SD','asd','asd'),(20,'-','qwe','Pria','qwe','123','','1888-10-06','jkt','SD','asd','asd');
 
 #
 # Structure for table "kriteria"
@@ -108,7 +108,7 @@ DROP TABLE IF EXISTS `kriteria`;
 CREATE TABLE `kriteria` (
   `id_kriteria` int(11) NOT NULL AUTO_INCREMENT,
   `kriteria` varchar(32) DEFAULT NULL,
-  `bobot` float(5,2) DEFAULT NULL,
+  `bobot` float(5,1) DEFAULT NULL,
   `type` varchar(12) DEFAULT NULL,
   PRIMARY KEY (`id_kriteria`)
 ) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
@@ -117,7 +117,30 @@ CREATE TABLE `kriteria` (
 # Data for table "kriteria"
 #
 
-INSERT INTO `kriteria` VALUES (57,'usia',25.00,'Core Factor'),(58,'gaji_perbulan',25.00,'Core Factor'),(59,'status_tempat_tinggal',20.00,'Core Factor'),(60,'pekerjaan',15.00,'Secondary Fa'),(61,'umur_motor',15.00,'Secondary Fa');
+INSERT INTO `kriteria` VALUES (57,'usia',25.0,'Core Factor'),(58,'gaji_perbulan',25.0,'Core Factor'),(59,'status_tempat_tinggal',20.0,'Core Factor'),(60,'pekerjaan',15.0,'Secondary Fa'),(61,'umur_motor',15.0,'Secondary Fa');
+
+#
+# Structure for table "match_"
+#
+
+DROP TABLE IF EXISTS `match_`;
+CREATE TABLE `match_` (
+  `id_match` int(11) NOT NULL AUTO_INCREMENT,
+  `id_calon_kr` int(11) DEFAULT NULL,
+  `id_subkriteria` int(11) DEFAULT NULL,
+  `nilai_gap` int(11) DEFAULT NULL,
+  `nilai_bobot` float(11,1) DEFAULT NULL,
+  `tanggal_lap` date DEFAULT NULL,
+  `kriteria` varchar(50) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_match`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+
+#
+# Data for table "match_"
+#
+
+INSERT INTO `match_` VALUES (16,14,1,-2,3.0,'2022-07-07','Usia','Suyono'),(17,14,1,-2,3.0,'2022-07-07','Gaji Perbulan','Suyono'),(18,14,1,-2,3.0,'2022-07-07','Status Tempat Tinggal','Suyono'),(19,14,5,2,3.5,'2022-07-07','Pekerjaan','Suyono'),(20,14,5,2,3.5,'2022-07-07','Umur Motor','Suyono'),(21,15,1,-2,3.0,'2022-07-07','Usia','Arief Wijaya'),(22,15,1,-2,3.0,'2022-07-07','Gaji Perbulan','Arief Wijaya'),(23,15,1,-2,3.0,'2022-07-07','Status Tempat Tinggal','Arief Wijaya'),(24,15,5,2,3.5,'2022-07-07','Pekerjaan','Arief Wijaya'),(25,15,5,2,3.5,'2022-07-07','Umur Motor','Arief Wijaya'),(26,16,1,-2,3.0,'2022-07-07','Usia','Joko Suprapto'),(27,16,1,-2,3.0,'2022-07-07','Gaji Perbulan','Joko Suprapto'),(28,16,1,-2,3.0,'2022-07-07','Status Tempat Tinggal','Joko Suprapto'),(29,16,5,2,3.5,'2022-07-07','Pekerjaan','Joko Suprapto'),(30,16,5,2,3.5,'2022-07-07','Umur Motor','Joko Suprapto');
 
 #
 # Structure for table "sub_kriteria"
@@ -128,7 +151,7 @@ CREATE TABLE `sub_kriteria` (
   `id_subkriteria` int(11) NOT NULL AUTO_INCREMENT,
   `id_kriteria` int(11) NOT NULL,
   `subkriteria` varchar(255) NOT NULL,
-  `nilai` float(10,2) NOT NULL,
+  `nilai` float(10,1) DEFAULT NULL,
   PRIMARY KEY (`id_subkriteria`)
 ) ENGINE=InnoDB AUTO_INCREMENT=144 DEFAULT CHARSET=latin1;
 
@@ -136,4 +159,4 @@ CREATE TABLE `sub_kriteria` (
 # Data for table "sub_kriteria"
 #
 
-INSERT INTO `sub_kriteria` VALUES (119,57,'17 - 23',1.00),(120,57,'24 - 26',3.00),(121,57,'27 - 35',5.00),(122,57,'36 - 45',4.00),(123,57,'46 - 55',2.00),(124,58,'2.500.000 – 3.499.000 ',1.00),(125,58,'3.500.000 – 5.499.000',2.00),(126,58,'5.500.000 – 7.999.000',3.00),(127,58,'8.000.000 – 15.000.000',4.00),(128,58,'15.000.000 – 50.000.000',5.00),(129,59,'Kost',1.00),(130,59,'Kontrak',2.00),(131,59,'Milik Keluarga',4.00),(132,59,'Milik Sendiri',5.00),(133,59,'Milik Orang Tua',3.00),(134,60,'PNS',5.00),(135,60,'karyawan Swasta',4.00),(136,60,'Wiraswasta',3.00),(137,60,'Profesional',2.00),(138,60,'Pensiunan',1.00),(139,61,'1 Tahun',5.00),(140,61,'2 – 3 Tahun',4.00),(141,61,'4 – 5 Tahun',3.00),(142,61,'6 Tahun',2.00),(143,61,'7 Tahun',1.00);
+INSERT INTO `sub_kriteria` VALUES (119,57,'17 - 23',1.0),(120,57,'24 - 26',3.0),(121,57,'27 - 35',5.0),(122,57,'36 - 45',4.0),(123,57,'46 - 55',2.0),(124,58,'2.500.000 – 3.499.000 ',1.0),(125,58,'3.500.000 – 5.499.000',2.0),(126,58,'5.500.000 – 7.999.000',3.0),(127,58,'8.000.000 – 15.000.000',4.0),(128,58,'15.000.000 – 50.000.000',5.0),(129,59,'Kost',1.0),(130,59,'Kontrak',2.0),(131,59,'Milik Keluarga',4.0),(132,59,'Milik Sendiri',5.0),(133,59,'Milik Orang Tua',3.0),(134,60,'PNS',5.0),(135,60,'karyawan Swasta',4.0),(136,60,'Wiraswasta',3.0),(137,60,'Profesional',2.0),(138,60,'Pensiunan',1.0),(139,61,'1 Tahun',5.0),(140,61,'2 – 3 Tahun',4.0),(141,61,'4 – 5 Tahun',3.0),(142,61,'6 Tahun',2.0),(143,61,'7 Tahun',1.0);
